5d2ea1d8ad5e64bd048d82b6a4b0766ab888b5c1
Thiago Bellini Ribeiro <thiago@bellini.dev>
2019-08-06 11:49:36 -0300

    Bump version to 0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, Zerosoft Tecnologia LTDA'
 author = 'Zerosoft Tecnologia LTDA'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
