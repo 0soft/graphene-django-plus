6954387e9c5a30510adbde102c9175bb65f45849
Thiago Bellini Ribeiro <thiago@bellini.dev>
2019-08-06 10:07:31 -0300

    Bump version to 0.6.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def _read(fname):
 
 setup(
     name='graphene-django-plus',
-    version='0.2.0.post0',
+    version='0.3.0',
     author='Zerosoft Tecnologia LTDA',
     author_email='admin@zerosoft.com.br',
     description='Tools to easily create permissioned CRUD endpoints in graphene.',
