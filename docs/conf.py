4364e912d328ed72867d74e84652b83c4c876e7e
Thiago Bellini Ribeiro <thiago@bellini.dev>
2019-10-17 20:02:25 -0300

    Bump to version 1.0
    The api is stable enough for a 1.0 release.

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, Zerosoft Tecnologia LTDA'
 author = 'Zerosoft Tecnologia LTDA'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
