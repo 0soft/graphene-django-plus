3a310ab22cd45884f0369c844c471177bfe254d9
Thiago Bellini Ribeiro <thiago@bellini.dev>
2020-04-25 11:38:12 -0300

    Base PermissionDenied on Django's

--- graphene_django_plus/exceptions.py
+++ graphene_django_plus/exceptions.py
@@ -1,5 +1,12 @@
-try:
-    from graphql_jwt.exceptions import PermissionDenied
-except ImportError:
-    class PermissionDenied(Exception):
-        default_message = "You do not have permission to perform this action"
+from django.core.exceptions import PermissionDenied as _PermissionDenied
+
+
+class PermissionDenied(_PermissionDenied):
+
+    default_message = "You do not have permission to perform this action"
+
+    def __init__(self, message=None):
+        if message is None:
+            message = self.default_message
+
+        super().__init__(message)
