7b77c22da4ab5b9c42f0b1198b88e17bcd45af6c
Thiago Bellini Ribeiro <thiago@bellini.dev>
2021-05-13 19:29:07 -0300

    Bump to version 2.4.3

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2019-2021, Zerosoft Tecnologia LTDA"
 author = "Zerosoft Tecnologia LTDA"
 
 # The full version, including alpha/beta/rc tags
-release = "2.4.2"
+release = "2.4.3"
 
 
 # -- General configuration ---------------------------------------------------
